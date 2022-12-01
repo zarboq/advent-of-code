ee2e1a9be22b7cc10134a1535d291d6de9cc180a
TurcFort07 <boramindas@gmail.com>
2022-12-01 18:05:40 +0100

    day1 done

---
+++ src/exercises/d1e1.rs
@@ -0,0 +1,27 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    if let Ok(lines) = read_lines("./src/resources/d1e1") {
+        let mut top_num_of_cals: i32 = 0;
+        let mut cur_elf_num_of_cals: i32 = 0;
+        for line in lines {
+            if line.as_ref().unwrap().len() == 0 {
+                if cur_elf_num_of_cals > top_num_of_cals {
+                    top_num_of_cals = cur_elf_num_of_cals;
+                }
+                cur_elf_num_of_cals = 0;
+            } else {
+                cur_elf_num_of_cals += line.unwrap().trim().parse::<i32>().unwrap();
+            }
+        }
+        println!("highest cal is: {}", top_num_of_cals);
+    }
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
---
+++ src/exercises/d1e2.rs
@@ -0,0 +1,30 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    if let Ok(lines) = read_lines("./src/resources/d1e1") {
+        let mut top3_arr: [i32; 250] = [0; 250];
+        let mut cur_elf_num_of_cals: i32 = 0;
+        let mut i = 0;
+        for line in lines {
+            if line.as_ref().unwrap().len() == 0 {
+                top3_arr[i] = cur_elf_num_of_cals;
+                i += 1;
+                cur_elf_num_of_cals = 0;
+
+            } else {
+                cur_elf_num_of_cals += line.unwrap().trim().parse::<i32>().unwrap();
+            }
+        }
+        top3_arr.sort_by(|a, b| b.cmp(a));
+        let sum = top3_arr[0] + top3_arr[1] + top3_arr[2];
+        println!("highest cal is: {}", sum);
+    }
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
